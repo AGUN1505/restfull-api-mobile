f3fc124e461e3221363bf3e7e2a33d3be20710e1
AGUN1505 <agun1505@gmail.com>
2024-07-02 11:06:36 +0700

    penambahan show update delete

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -18,6 +18,16 @@ class Api extends BaseController
         return $this->respond(['message' => 'success', 'data' => $data]);
     }
 
+    public function show($id = null)
+    {
+        $model = new BeritaModel();
+        $data = $model->where('id', $id)->first();
+        if (is_null($data)) {
+            return $this->failNotFound('data not found');
+        }
+        return $this->respond(['message' => 'success', 'data' => $data]);
+    }
+
     public function create()
     {
         $model = new BeritaModel();
@@ -35,5 +45,43 @@ class Api extends BaseController
             ]
         ];
         return $this->respond(['pesan' => 'berhasil', 'data' => $data]);
+    }
+
+    public function update($id = null)
+    {
+        $model = new BeritaModel();
+        $data = [
+            'judul' => $this->request->getVar('judul'),
+            'isi' => $this->request->getVar('isi'),
+            'gambar' => $this->request->getVar('gambar')
+        ];
+        $model->update($id, $data);
+        $response = [
+            'status'   => 200,
+            'error'    => null,
+            'messages' => [
+                'success' => 'Data Updated'
+            ]
+        ];
+        return $this->respond($response);
+    }
+
+    public function delete($id = null)
+    {
+        $model = new BeritaModel();
+        $data = $model->find($id);
+        if ($data) {
+            $model->delete($id);
+            $response = [
+                'status'   => 200,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Data Deleted'
+                ]
+            ];
+            return $this->respondDeleted($response);
+        } else {
+            return $this->failNotFound('data not found');
+        }
     }
 }
